4968f50dbcdf970f98c57f340d1ce92b905038e5
retxxxirt <retxxxirt@gmail.com>
2020-03-28 00:44:36 +0800

    Added setup.py file.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='django-runscript',
+    packages=['django_runscript'],
+    package_data={'django_runscript': ['management/commands/*.py']},
+    version='0.1.0',
+    license='MIT',
+    description='Django command for running custom scripts with django environment.',
+    author='retxxxirt',
+    author_email='retxxirt@gmail.com',
+    url='https://github.com/retxxxirt/django-runscript',
+    keywords=['django runscript', 'django', 'django scripts', 'django management'],
+    tests_require=(tests_require := ['django>=3.0,<3.1']),
+    extras_require={'test': tests_require}
+)
