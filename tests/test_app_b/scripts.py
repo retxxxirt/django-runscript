7da730292544c86ccc201ad2fc1f4b9daba884f3
retxxxirt <retxxxirt@gmail.com>
2020-03-28 09:52:16 +0800

    Changed lookup policy.

---
+++ tests/test_app_b/scripts.py
@@ -0,0 +1,6 @@
+def run_script_a():
+    print(1)
+
+
+def run_script_b():
+    raise Exception()
--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,24 +1,31 @@
 from collections import Callable
 
+from django.conf import settings
 from django.test import TestCase
 
 from django_runscript import utilities
 
 
 class UtilitiesTestCase(TestCase):
+    def test_apps_lookup(self):
+        self.assertEqual(utilities.apps_lookup('%s'), settings.INSTALLED_APPS)
+
     def test_import_script(self):
-        self.assertIsInstance(utilities.import_script('test_app.script_a'), Callable)
-        self.assertIsInstance(utilities.import_script('tests.test_app.script_a'), Callable)
+        self.assertIsInstance(utilities.import_script('script_a'), Callable)
+        self.assertIsInstance(utilities.import_script('test_app_a.script_a'), Callable)
+        self.assertIsInstance(utilities.import_script('tests.test_app_a.script_a'), Callable)
+        self.assertIsInstance(utilities.import_script('test_app_a.scripts.script_a'), Callable)
+        self.assertIsInstance(utilities.import_script('tests.test_app_a.scripts.script_a'), Callable)
 
-        self.assertRaises(ImportError, utilities.import_script, 'test_app.script_c')
-        self.assertRaises(ImportError, utilities.import_script, 'tests.test_app.scripts.script_a')
+        self.assertIsInstance(utilities.import_script('run_script_a'), Callable)
+        self.assertIsInstance(utilities.import_script('test_app_b.run_script_a'), Callable)
+        self.assertIsInstance(utilities.import_script('tests.test_app_b.run_script_a'), Callable)
+        self.assertIsInstance(utilities.import_script('test_app_b.scripts.run_script_a'), Callable)
+        self.assertIsInstance(utilities.import_script('tests.test_app_b.scripts.run_script_a'), Callable)
 
-    def test_import_script_lookup(self):
-        self.assertIsInstance(utilities.import_script_lookup('script_a'), Callable)
-        self.assertIsInstance(utilities.import_script_lookup('test_app.script_a'), Callable)
-        self.assertIsInstance(utilities.import_script_lookup('tests.test_app.script_a'), Callable)
-        self.assertIsInstance(utilities.import_script_lookup('test_app.scripts.script_a'), Callable)
-        self.assertIsInstance(utilities.import_script_lookup('tests.test_app.scripts.script_a'), Callable)
+        self.assertRaises(ImportError, utilities.import_script, 'scripts.script_a')
+        self.assertRaises(ImportError, utilities.import_script, 'test_app_a.script_c')
+        self.assertRaises(ImportError, utilities.import_script, 'test_app_b.script_a')
 
-        self.assertRaises(ImportError, utilities.import_script_lookup, 'scripts.script_a')
-        self.assertRaises(ImportError, utilities.import_script_lookup, 'test_app.script_c')
+        self.assertRaises(ImportError, utilities.import_script, 'scripts.run_script_a')
+        self.assertRaises(ImportError, utilities.import_script, 'test_app_b.run_script_c')
